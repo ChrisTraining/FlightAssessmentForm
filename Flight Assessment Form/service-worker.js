4945436e02ac89e2ba3fb01f4bf82ef29619160d
ChrisTraining <chris.thompson@heliguy.com>
2025-06-02 09:51:39 +0100

    Add files via upload

--- Flight Assessment Form/service-worker.js
+++ Flight Assessment Form/service-worker.js
@@ -1,29 +1,32 @@
 const CACHE_NAME = "flight-assessment-v1";
-const urlsToCache = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./service-worker.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
-  // add additional files below
+  "./styles.css", // include your CSS if any
+  "./main.js",    // include your JS if any
   "./signatures/jack.png",
   "./signatures/chris.png",
   "./signatures/bradley.png",
-  "./signatures/odysseus.png"
+  "./signatures/odysseus.png",
+  "./libs/html2pdf.bundle.min.js",
+  "./libs/jspdf.umd.min.js",
+  "./libs/jspdf.plugin.autotable.min.js",
+  "./libs/html2pdf.bundle.min.js"
+
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
